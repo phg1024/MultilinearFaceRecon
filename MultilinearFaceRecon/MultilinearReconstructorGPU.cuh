a345e885b7a50ec42656e1fa53cc94ea5bc50dad
Peihong Guo <peihongguo@gmail.com>
2013-12-18 22:34:03 -0500

    CUDA implementation in progress

---
+++ MultilinearFaceRecon/MultilinearReconstructorGPU.cuh
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "Math/Tensor.hpp"
+#include "Geometry/point.hpp"
+#include "Geometry/matrix.hpp"
+#include "Geometry/geometryutils.hpp"
+
+class MultilinearReconstructorGPU 
+{
+public:
+	MultilinearReconstructorGPU();
+	~MultilinearReconstructorGPU();
+
+private:
+	// host resources
+
+private:
+	// device resources
+};
